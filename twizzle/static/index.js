5daf29a4256a739d7a40cec07403ce7bdbfbeb1a
Mads Frandsen <madschristianfrandsen@gmail.com>
2023-06-09 23:02:40 +0200

    follow implementation done

--- twizzle/static/index.js
+++ twizzle/static/index.js
@@ -16,20 +16,55 @@ function like(postId) {
 }
 
 
+// function follow(userId) {
+//   const followCount = document.querySelector('#profile-number-followers');
+//   const followButton = document.querySelector('#follow-btn');
+
+//   fetch(`/user/follow/${userId}`, { method: "POST" })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       followCount.innerHTML = data.followers;
+//       if (data.followed) {
+//         followButton.textContent = "Unfollow";
+//         followButton.classList.remove("follow-btn");
+//         followButton.classList.add("unfollow-btn");
+//       } else {
+//         followButton.textContent = "Follow";
+//         followButton.classList.remove("unfollow-btn");
+//         followButton.classList.add("follow-btn");
+//       }
+//     })
+//     .catch((e) => alert("Could not follow user. Are you logged in?"));
+// }
+
 function follow(userId) {
-  const followCount = document.getElementById(`profile-label-followers`);
-  const followButton = document.getElementById(`follow-btn`);
+  const followCount = document.querySelector('#profile-number-followers');
+  const followButton = document.querySelector('#follow-btn');
 
   fetch(`/user/follow/${userId}`, { method: "POST" })
-    .then((res) => res.json())
-    .then((data) => {
-      followCount.innerHTML = data['followers'];
-      if (data["followed"]) {
-        followButton.className = "follow-btn";
+    .then((res) => {
+      console.log(res); 
+      if (res.ok) {
+        return res.json();
       } else {
-        followButton.className = "unfollow-btn";
+        throw new Error("Not logged in");
       }
     })
-    .catch((e) => alert("Could not follow user. Are you logged in?"));
+    .then((data) => {
+      followCount.innerHTML = data.followers;
+      if (data.followed) {
+        followButton.textContent = "Unfollow";
+        followButton.classList.remove("follow-btn");
+        followButton.classList.add("unfollow-btn");
+      } else {
+        followButton.textContent = "Follow";
+        followButton.classList.remove("unfollow-btn");
+        followButton.classList.add("follow-btn");
+      }
+    })
+    .catch((error) => {
+      alert("Could not follow user. " + error.message);
+    });
 }
+
 
--- twizzle/users/routes.py
+++ twizzle/users/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 import os
 from flask import render_template, url_for, flash, redirect, request, abort, current_app, jsonify
 from twizzle import bcrypt
-from twizzle.queries import insert_user, get_user_by_email, update_user_info, get_some_posts_by_user, get_user_post_count, get_user_by_name, update_user_password, has_user_liked_post, get_likes_for_post, get_followers_count_for_user, get_following_count_for_user, does_user_follow, follow_user, unfollow_user
+from twizzle.queries import insert_user, get_user_by_email, update_user_info, get_some_posts_by_user, get_user_post_count, get_user_by_name, update_user_password, has_user_liked_post, get_likes_for_post, get_followers_count_for_user, get_following_count_for_user, does_user_follow, follow_user, unfollow_user, get_user_by_id
 from twizzle.users.forms import RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
 from twizzle.users.utils import save_picture, send_reset_email
 from twizzle.models import User
@@ -100,12 +100,20 @@ def user_posts(user_name):
     
     following = get_following_count_for_user(user.id)
     followers = get_followers_count_for_user(user.id)
+    follow = {}
+    follow['following'] = following
+    follow['followers'] = followers
+
+    if current_user.is_authenticated:
+        followed = does_user_follow(user.id, current_user.id)
+        follow['followed'] = followed
+
 
     pages = [i for i in range(1, total_pages+1)]
     return render_template('user_posts.html', posts=posts, user=user, 
                            page=page, total_pages=total_pages, 
                            pages=pages, total_posts=total_posts['count'],
-                           following=following, followers=followers)
+                           follow=follow)
 
 
 
@@ -141,11 +149,11 @@ def reset_token(token):
     return render_template('reset_token.html', title='Reset Password', form=form)
 
 
-@users.route("/user/follow/<string:user_name>", methods=['POST'])
+@users.route("/user/follow/<int:user_id>", methods=['POST'])
 @login_required
-def follow_user(user_name):
-    user = get_user_by_name(user_name)
-    follow = does_user_follow(current_user.id)
+def followUser(user_id):
+    user = get_user_by_id(user_id)
+    follow = does_user_follow(user.id, current_user.id)
     if not user:
         return jsonify({'error': 'User does not exist.'}, 400)
     elif follow:
@@ -154,5 +162,5 @@ def follow_user(user_name):
         follow_user(user.id, current_user.id)
     
     followers = get_followers_count_for_user(user.id)
-    followed = does_user_follow(current_user.id)
+    followed = does_user_follow(user.id, current_user.id)
     return jsonify({"followers": followers, "followed": followed})
